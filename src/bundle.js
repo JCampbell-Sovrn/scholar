b57ef0d2901d71d75c130e561eca14b2431faa88
Jeremy <76131636+JCampbell-Sovrn@users.noreply.github.com>
2024-08-16 13:10:56 -0600

    updated bundle

--- src/bundle.js
+++ src/bundle.js
@@ -58,32 +58,15 @@
     var _jsxFileName = "/Users/jcampbell/Documents/GitHub/scholar/src/index.js";
     /*
     
-     MIT License
-    
-     Copyright (c) 2022 Looker Data Sciences, Inc.
-    
-     Permission is hereby granted, free of charge, to any person obtaining a copy
-     of this software and associated documentation files (the "Software"), to deal
-     in the Software without restriction, including without limitation the rights
-     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-     copies of the Software, and to permit persons to whom the Software is
-     furnished to do so, subject to the following conditions:
-    
-     The above copyright notice and this permission notice shall be included in all
-     copies or substantial portions of the Software.
-    
-     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-     SOFTWARE.
+    INDEX.JS
     
      */
     
     
     
+    
+    
+    // Import scholar.js to include it in the bundle
     
     window.addEventListener('DOMContentLoaded', event => {
       var root = document.createElement('div');
@@ -93,7 +76,7 @@
         __self: undefined,
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 35,
+          lineNumber: 18,
           columnNumber: 19
         }
       }), root);
@@ -61775,7 +61758,7 @@
     /******/ 	
     /******/ 	/* webpack/runtime/getFullHash */
     /******/ 	(() => {
-    /******/ 		__webpack_require__.h = () => ("a75d4094b1bafe241848")
+    /******/ 		__webpack_require__.h = () => ("1cd4a396fde83b43ed8b")
     /******/ 	})();
     /******/ 	
     /******/ 	/* webpack/runtime/global */
