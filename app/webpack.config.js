105265c6c6d33e7681c7378bb6c2b5ff3107bdca
Jeremy <76131636+JCampbell-Sovrn@users.noreply.github.com>
2024-08-16 14:14:33 -0600

    updated folders

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,27 +2,29 @@
  * webpack.config
  */
 
-const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path')
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
 const PATHS = {
-  app: path.join(__dirname, 'src/index.js')
-};
+  app: path.join(__dirname, 'src/index.js'),
+}
 
 module.exports = {
   entry: {
     app: PATHS.app,
-    scholar: PATHS.scholar, // Add this line
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'scholar.bundle.js', // This will create app.bundle.js and scholar.bundle.js
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
         include: /src/,
         sideEffects: false,
       },
@@ -38,4 +40,4 @@ module.exports = {
       analyzerMode: process.env.ANALYZE_MODE || 'disabled',
     }),
   ],
-};
+}
